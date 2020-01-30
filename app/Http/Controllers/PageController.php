b16b94da86c3bc030c331cf4b534f1d0cf8fc24c
Casper Herse <casp095a@gmail.com>
2020-01-30 23:22:27 +0100

    Removed unused frameworks

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,11 +3,7 @@
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Controller as BaseController;
 
 class PageController extends Controller
 {
